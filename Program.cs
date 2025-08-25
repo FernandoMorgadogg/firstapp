bdd606dcee4014053e4c058960c32d26c4db03be
FernandoMorgadogg <fernando.morgadogg@outlook.com>
2025-08-25 08:41:56 -0300

    ajuste

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -26,33 +24,9 @@ if (app.Environment.IsDevelopment())
 {
     var process = new System.Diagnostics.Process();
     process.StartInfo.FileName = "open";
-    process.StartInfo.Arguments = "https://localhost:7000/swagger";
+    process.StartInfo.Arguments = "http://localhost:5000/swagger";
     process.Start();
 }
 
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
